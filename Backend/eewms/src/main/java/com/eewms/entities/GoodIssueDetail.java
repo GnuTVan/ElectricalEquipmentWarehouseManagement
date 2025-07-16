53a2599540ecc8421e0998dcb1809fd6e63d331b
YukineDL <phamhoangnam2406@gmail.com>
2025-07-16 23:44:11 +0700

    Initialize Order and Goodnote

---
+++ Backend/eewms/src/main/java/com/eewms/entities/GoodIssueDetail.java
@@ -0,0 +1,33 @@
+package com.eewms.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "good_issue_details")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GoodIssueDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ginDetailsId;
+
+    @ManyToOne
+    @JoinColumn(name = "gin_id", nullable = false)
+    private GoodIssueNote goodIssueNote;
+
+//    @ManyToOne
+//    @JoinColumn(name = "warehouse_id", nullable = false)
+//    private Warehouse warehouse;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = true)
+    private Product product;
+
+    @Column(nullable = false)
+    private Double quantity;
+}
