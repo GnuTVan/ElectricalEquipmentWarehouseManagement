233c26e3513c610936d3d01d6b0c0790a53718e3
YukineDL <phamhoangnam2406@gmail.com>
2025-07-18 18:21:10 +0700

    hiding

--- Backend/eewms/src/main/java/com/eewms/repository/CustomerRepository.java
+++ Backend/eewms/src/main/java/com/eewms/repository/CustomerRepository.java
@@ -1,16 +1,16 @@
-package com.eewms.repository;
-
-import com.eewms.entities.Customer;
-
-import java.util.Collection;
-import java.util.Optional;
-
-public interface CustomerRepository {
-    Customer save(Customer entity);
-
-    Optional<Customer> findById(Long id);
-
-    Collection<Customer> findAll();
-
-    void deleteById(Long id);
-}
+//package com.eewms.repository;
+//
+//import com.eewms.entities.Customer;
+//
+//import java.util.Collection;
+//import java.util.Optional;
+//
+//public interface CustomerRepository {
+//    Customer save(Customer entity);
+//
+//    Optional<Customer> findById(Long id);
+//
+//    Collection<Customer> findAll();
+//
+//    void deleteById(Long id);
+//}
