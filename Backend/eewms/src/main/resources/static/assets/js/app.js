78121a9fac76e479f53ad52dc3b350de1ce539cd
Dinzy1712 <justdinzy02@gmail.com>
2025-08-14 02:48:26 +0700

    Redundant JS

--- Backend/eewms/src/main/resources/static/assets/js/app.js
+++ Backend/eewms/src/main/resources/static/assets/js/app.js
@@ -21,11 +21,9 @@
  ================================*/
 
 
- window.onload = function loader() {
+window.onload = function loader() {
     // Preloader
     setTimeout(() => {
-        document.getElementById('preloader').style.visibility = 'hidden';
-        document.getElementById('preloader').style.opacity = '0';
     }, 350);
 
     // Menus
@@ -59,7 +57,8 @@ function getClosest(elem, selector) {
             function (s) {
                 var matches = (this.document || this.ownerDocument).querySelectorAll(s),
                     i = matches.length;
-                while (--i >= 0 && matches.item(i) !== this) { }
+                while (--i >= 0 && matches.item(i) !== this) {
+                }
                 return i > -1;
             };
     }
@@ -139,36 +138,18 @@ function activateSidebarMenu() {
     }
 }
 
-// function activateSidebarMenu() {
-//     var current = location.pathname.substring(location.pathname.lastIndexOf('/') + 1);
-//     if (current !== "" && document.getElementById("sidebar")){
-//         var menuItems = document.querySelectorAll('#sidebar a');
-//         for (var i = 0, len = menuItems.length; i < len; i++) {
-//             if (menuItems[i].getAttribute("href").indexOf(current) !== -1) {
-//                 menuItems[i].parentElement.className += " active";
-//                 if(menuItems[i].closest(".sidebar-submenu")) {
-//                     menuItems[i].closest(".sidebar-submenu").classList.add("d-block");
-//                 }
-//                 if(menuItems[i].closest(".sidebar-dropdown")) {
-//                     menuItems[i].closest(".sidebar-dropdown").classList.add("active");
-//                 }
-//             }
-//         }
-//     }
-// }
-
-if(document.getElementById("close-sidebar")){
-    document.getElementById("close-sidebar").addEventListener("click", function() {
+if (document.getElementById("close-sidebar")) {
+    document.getElementById("close-sidebar").addEventListener("click", function () {
         document.getElementsByClassName("page-wrapper")[0].classList.toggle("toggled");
     });
 }
 
 // Clickable Menu
-if(document.getElementById("navigation")){
+if (document.getElementById("navigation")) {
     var elements = document.getElementById("navigation").getElementsByTagName("a");
-    for(var i = 0, len = elements.length; i < len; i++) {
+    for (var i = 0, len = elements.length; i < len; i++) {
         elements[i].onclick = function (elem) {
-            if(elem.target.getAttribute("href") === "javascript:void(0)") {
+            if (elem.target.getAttribute("href") === "javascript:void(0)") {
                 var submenu = elem.target.nextElementSibling.nextElementSibling;
                 submenu.classList.toggle('open');
             }
@@ -176,11 +157,11 @@ if(document.getElementById("navigation")){
     }
 }
 
-if(document.getElementById("sidebar")){
+if (document.getElementById("sidebar")) {
     var elements = document.getElementById("sidebar").getElementsByTagName("a");
-    for(var i = 0, len = elements.length; i < len; i++) {
+    for (var i = 0, len = elements.length; i < len; i++) {
         elements[i].onclick = function (elem) {
-            if(elem.target.getAttribute("href") === "javascript:void(0)") {
+            if (elem.target.getAttribute("href") === "javascript:void(0)") {
                 elem.target.parentElement.classList.toggle("active");
                 elem.target.nextElementSibling.classList.toggle("d-block");
             }
@@ -191,12 +172,12 @@ if(document.getElementById("sidebar")){
 // Menu sticky
 function windowScroll() {
     var navbar = document.getElementById("topnav");
-    if(navbar === null) {
-        
-    }else if( document.body.scrollTop >= 50 ||
-    document.documentElement.scrollTop >= 50){
+    if (navbar === null) {
+
+    } else if (document.body.scrollTop >= 50 ||
+        document.documentElement.scrollTop >= 50) {
         navbar.classList.add("nav-sticky");
-    }else {
+    } else {
         navbar.classList.remove("nav-sticky");
     }
 }
@@ -213,11 +194,11 @@ window.onscroll = function () {
 
 function scrollFunction() {
     var mybutton = document.getElementById("back-to-top");
-    if(mybutton === null) {
-        
-    }else if( document.body.scrollTop > 500 || document.documentElement.scrollTop > 500){
+    if (mybutton === null) {
+
+    } else if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
         mybutton.style.display = "block";
-    }else {
+    } else {
         mybutton.style.display = "none";
     }
 }
@@ -231,9 +212,9 @@ function topFunction() {
 feather.replace();
 
 // dd-menu
-if(document.getElementsByClassName("dd-menu")) {
+if (document.getElementsByClassName("dd-menu")) {
     var ddmenu = document.getElementsByClassName("dd-menu");
-    for(var i = 0, len = ddmenu.length; i < len; i++) {
+    for (var i = 0, len = ddmenu.length; i < len; i++) {
         ddmenu[i].onclick = function (elem) {
             elem.stopPropagation();
         }
@@ -242,7 +223,8 @@ if(document.getElementsByClassName("dd-menu")) {
 
 //ACtive Sidebar
 (function () {
-    var current = location.pathname.substring(location.pathname.lastIndexOf('/') + 1);;
+    var current = location.pathname.substring(location.pathname.lastIndexOf('/') + 1);
+    ;
     if (current === "") return;
     var menuItems = document.querySelectorAll('.sidebar-nav a');
     for (var i = 0, len = menuItems.length; i < len; i++) {
@@ -257,29 +239,29 @@ if(document.getElementsByClassName("dd-menu")) {
 (function () {
     'use strict'
 
-    if(document.getElementsByClassName('needs-validation').length > 0) {
+    if (document.getElementsByClassName('needs-validation').length > 0) {
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
         var forms = document.querySelectorAll('.needs-validation')
 
         // Loop over them and prevent submission
         Array.prototype.slice.call(forms)
             .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
-                }
+                form.addEventListener('submit', function (event) {
+                    if (!form.checkValidity()) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
 
-                form.classList.add('was-validated')
-            }, false)
-        })
+                    form.classList.add('was-validated')
+                }, false)
+            })
     }
 })();
 
 //Tooltip
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
+    return new bootstrap.Tooltip(tooltipTriggerEl)
 });
 
 //Switcher
@@ -288,5 +270,16 @@ try {
         document.getElementById('theme-opt').href = '../assets/css/' + theme + '.min.css';
     };
 } catch (error) {
-    
+
 }
+
+const btn = document.getElementById("userMenuBtn");
+const dropdown = document.getElementById("userMenuDropdown");
+btn?.addEventListener("click", () => {
+    dropdown?.classList.toggle("hidden");
+});
+document.addEventListener("click", (event) => {
+    if (!event.target.closest("#userMenuWrapper")) {
+        dropdown?.classList.add("hidden");
+    }
+});
