99f9c0bc1dde08a580035f34bd0c4253ca39bebe
Dinzy1712 <justdinzy02@gmail.com>
2025-08-20 19:40:14 +0700

    UI Overhaul

--- Backend/eewms/src/main/java/com/eewms/constant/PurchaseOrderStatus.java
+++ Backend/eewms/src/main/java/com/eewms/constant/PurchaseOrderStatus.java
@@ -1,9 +1,19 @@
 package com.eewms.constant;
 
+import lombok.Getter;
+
+@Getter
 public enum PurchaseOrderStatus {
-    CHO_DUYET,
-    CHO_GIAO_HANG,      // Chờ giao hàng
-    DA_GIAO_MOT_PHAN,   // Đã giao một phần
-    HOAN_THANH,         // Hoàn thành
-    HUY                 // Đã huỷ
+    CHO_DUYET("Chờ duyệt"), // Chờ duyệt
+    CHO_GIAO_HANG("Chờ giao hàng"),      // Chờ giao hàng
+    DA_GIAO_MOT_PHAN("Đã giao một phần"),   // Đã giao một phần
+    HOAN_THANH("Hoàn thành"),         // Hoàn thành
+    HUY("Hủy");             // Đã huỷ
+    
+    private final String label;
+
+    PurchaseOrderStatus(String label) {
+        this.label = label;
+    }
+
 }
